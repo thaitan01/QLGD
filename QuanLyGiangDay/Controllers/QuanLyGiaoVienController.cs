0aff86dc3a18518b67dee03fbb33a70c25a59ef2
huynhminhtuan-ct <hmtuana18122@cusc.ctu.edu.vn>
2022-10-03 21:31:59 +0700

    add controllo

---
+++ QuanLyGiangDay/Controllers/QuanLyGiaoVienController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyGiangDay.Models.EF;
+
+namespace QuanLyGiangDay.Controllers
+{
+    public class QuanLyGiaoVienController : Controller
+    {
+        private quanlygiaovienEntities db = new quanlygiaovienEntities();
+
+        // GET: QuanLyGiaoVien
+        public ActionResult Index()
+        {
+            var giaoViens = db.GiaoViens.Include(g => g.LoaiGV);
+            return View(giaoViens.ToList());
+        }
+
+        // GET: QuanLyGiaoVien/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiaoVien giaoVien = db.GiaoViens.Find(id);
+            if (giaoVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giaoVien);
+        }
+
+        // GET: QuanLyGiaoVien/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaLoaiGV = new SelectList(db.LoaiGVs, "MaLoaiGV", "TenLoaiGV");
+            return View();
+        }
+
+        // POST: QuanLyGiaoVien/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaGV,TenGV,MaLoaiGV,Email,Sdt")] GiaoVien giaoVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GiaoViens.Add(giaoVien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaLoaiGV = new SelectList(db.LoaiGVs, "MaLoaiGV", "TenLoaiGV", giaoVien.MaLoaiGV);
+            return View(giaoVien);
+        }
+
+        // GET: QuanLyGiaoVien/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiaoVien giaoVien = db.GiaoViens.Find(id);
+            if (giaoVien == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaLoaiGV = new SelectList(db.LoaiGVs, "MaLoaiGV", "TenLoaiGV", giaoVien.MaLoaiGV);
+            return View(giaoVien);
+        }
+
+        // POST: QuanLyGiaoVien/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaGV,TenGV,MaLoaiGV,Email,Sdt")] GiaoVien giaoVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(giaoVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaLoaiGV = new SelectList(db.LoaiGVs, "MaLoaiGV", "TenLoaiGV", giaoVien.MaLoaiGV);
+            return View(giaoVien);
+        }
+
+        // GET: QuanLyGiaoVien/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiaoVien giaoVien = db.GiaoViens.Find(id);
+            if (giaoVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giaoVien);
+        }
+
+        // POST: QuanLyGiaoVien/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            GiaoVien giaoVien = db.GiaoViens.Find(id);
+            db.GiaoViens.Remove(giaoVien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

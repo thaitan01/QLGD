18ef9148b3880e6798ba3a19d663f53b42762af5
Phuoc Thinh <tpthinh.0160@vitenet.net>
2022-10-06 14:39:29 +0900

    add file

---
+++ QuanLyGiangDay/Content/_custom/datatables.js
@@ -0,0 +1,10 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    $('#dataTable').DataTable(
+        {
+            language: {
+                url: '//cdn.datatables.net/plug-ins/1.12.1/i18n/vi.json'
+            }
+
+        });
+});

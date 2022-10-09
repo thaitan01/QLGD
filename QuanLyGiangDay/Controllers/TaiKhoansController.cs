ab2e6df3ec333ceaa4058118e17c66ce86c23e95
qtdata18113 <qtdata18113@cusc.ctu.edu.vn>
2022-10-09 17:49:27 +0700

    chuc nang quan ly tai khoan

---
+++ QuanLyGiangDay/Controllers/TaiKhoansController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyGiangDay.Models.EF;
+using System.Web.Helpers;
+
+namespace QuanLyGiangDay.Controllers
+{
+    public class TaiKhoansController : Controller
+    {
+        private quanlygiaovienEntities db = new quanlygiaovienEntities();
+
+        // GET: TaiKhoans
+        public ActionResult Index()
+        {
+            var taiKhoan = db.TaiKhoan.Include(t => t.VaiTro);
+            return View(taiKhoan.ToList());
+        }
+
+        // GET: TaiKhoans/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        public ActionResult _PartialDetail(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_PartialDetail",taiKhoan);
+        }
+
+        // GET: TaiKhoans/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaVT = new SelectList(db.VaiTro, "MaVT", "TenVT");
+            return View();
+        }
+
+        public ActionResult _PartialCreate()
+        {
+            ViewBag.MaVT = new SelectList(db.VaiTro, "MaVT", "TenVT");
+            return PartialView("_PartialCreate");
+        }
+
+        // POST: TaiKhoans/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaTK,TenDN,MatKhau,MaGV,MaVT,HoTen")] TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                db.TaiKhoan.Add(taiKhoan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaVT = new SelectList(db.VaiTro, "MaVT", "TenVT", taiKhoan.MaVT);
+            return View(taiKhoan);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult _PartialCreate([Bind(Include = "MaTK,TenDN,MatKhau,MaGV,MaVT,HoTen")] TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                taiKhoan.MatKhau = Crypto.HashPassword(taiKhoan.MatKhau);
+                db.TaiKhoan.Add(taiKhoan);
+                db.SaveChanges();
+                return Json(new { Success = true });
+            }
+
+            return Json(new { Success = false, Message = "Tài khoản đã tồn tại!không thể thêm mới" });
+        }
+
+        // GET: TaiKhoans/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaVT = new SelectList(db.VaiTro, "MaVT", "TenVT", taiKhoan.MaVT);
+            return View(taiKhoan);
+        }
+
+        public ActionResult _PartialEdit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaVT = new SelectList(db.VaiTro, "MaVT", "TenVT", taiKhoan.MaVT);
+            return PartialView("_PartialEdit", taiKhoan);
+        }
+
+        // POST: TaiKhoans/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaTK,TenDN,MatKhau,MaGV,MaVT,HoTen")] TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taiKhoan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaVT = new SelectList(db.VaiTro, "MaVT", "TenVT", taiKhoan.MaVT);
+            return View(taiKhoan);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult _PartialEdit([Bind(Include = "MaTK,TenDN,MatKhau,MaGV,MaVT,HoTen")] TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taiKhoan).State = EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { Success = true });
+            }
+            ViewBag.MaVT = new SelectList(db.VaiTro, "MaVT", "TenVT", taiKhoan.MaVT);
+            return Json(new { Success = false, Message = "Lỗi! Không thể cập nhật tài khoản này" });
+        }
+
+        // GET: TaiKhoans/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        public ActionResult _PartialDelete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_PartialDelete", taiKhoan);
+        }
+
+        // POST: TaiKhoans/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            db.TaiKhoan.Remove(taiKhoan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost, ActionName("_PartialDelete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult _PartialDeleteConfirmed(string id)
+        {
+            TaiKhoan taiKhoan = db.TaiKhoan.Find(id);
+            db.TaiKhoan.Remove(taiKhoan);
+            db.SaveChanges();
+            return Json(new { Success = true });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

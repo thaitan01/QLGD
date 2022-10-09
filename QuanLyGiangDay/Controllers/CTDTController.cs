1ee4c807f21e7d54794d7363a2e914379138c1b4
huynhminhtuan-ct <hmtuana18122@cusc.ctu.edu.vn>
2022-10-09 18:43:55 +0700

    dd

---
+++ QuanLyGiangDay/Controllers/CTDTController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyGiangDay.Models.EF;
+
+namespace QuanLyGiangDay.Controllers
+{
+    public class CTDTController : Controller
+    {
+        private quanlygiaovienEntities db = new quanlygiaovienEntities();
+
+        // GET: CTDT
+        public ActionResult Index()
+        {
+            return View(db.CTDT.ToList());
+        }
+
+        // GET: CTDT/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTDT cTDT = db.CTDT.Find(id);
+            if (cTDT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTDT);
+        }
+
+        // GET: CTDT/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CTDT/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaCTDT,TenCTDT,MoTa")] CTDT cTDT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CTDT.Add(cTDT);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cTDT);
+        }
+
+        // GET: CTDT/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTDT cTDT = db.CTDT.Find(id);
+            if (cTDT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTDT);
+        }
+
+        // POST: CTDT/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaCTDT,TenCTDT,MoTa")] CTDT cTDT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cTDT).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cTDT);
+        }
+
+        // GET: CTDT/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CTDT cTDT = db.CTDT.Find(id);
+            if (cTDT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cTDT);
+        }
+
+        // POST: CTDT/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            CTDT cTDT = db.CTDT.Find(id);
+            db.CTDT.Remove(cTDT);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

b50fd479b204e7df803e02d008a3d0ec2ea8b865
qtdata18113 <qtdata18113@cusc.ctu.edu.vn>
2022-10-15 19:14:48 +0700

    sua loi 15/10/2022

--- QuanLyGiangDay/Scripts/myScript.js
+++ QuanLyGiangDay/Scripts/myScript.js
@@ -1,18 +1,22 @@
-﻿$(function () {
+﻿
+$(function () {
     $.ajaxSetup({ cache: false });
     $("a[data-modal]").on("click", function (e) {
+        changLoading(true);
         $('#myModalContent').load(this.href, function () {
             $('#myModal').modal({
                 keyboard: true
             }, 'show');
+            changLoading(false);
             bindForm(this);
         });
         return false;
     });
 });
+
 function bindForm(dialog) {
     $('form', dialog).submit(function () {
-        $('#progress').show();
+        changLoading(true);
         $.ajax({
             url: this.action,
             type: this.method,
@@ -20,20 +24,17 @@ function bindForm(dialog) {
             success: function (result) {
                 if (result.Success) {
                     $('#myModal').modal('hide');
-                    $('#progress').hide();
                     location.reload();
-
                 } else {
-                    $('#progress').hide();
                     if (result.Message != null) {
                         alert(result.Message);
                     }
-
                     bindForm();
-
                 }
+                changLoading(false);
             }
         });
         return false;
     });
 }
+
